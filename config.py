a8e8e16a9b40e3e86ee1e3d848411791635c16d2
GDNBHARATH448 <gollapallibharath24@gmail.com>
2024-12-31 13:21:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "28362850"))
 API_HASH = getenv("API_HASH", "34f9cb93364db16fc45d003e4c81d97a")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7820751181:AAFDMs4SVMkyjv23Wem_jHm1w254OwCCNO8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7717871336:AAGcjAkSp5qkq8MASPbePOQn2NtdbbUw2c4")
 # Get your MongoDB URI from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 
