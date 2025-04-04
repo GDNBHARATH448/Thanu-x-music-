961bf83ef0c691e4a67ec85df968cbb0299d395b
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-04-05 00:05:21 +0530

    Update thumbnails.py

--- Dfschinnamusic/utils/thumbnails.py
+++ Dfschinnamusic/utils/thumbnails.py
@@ -2,14 +2,14 @@ from youtubesearchpython.__future__ import VideosSearch
 
 
 
-async def gen_thumb(videoid):
+async def gen_qthumb(videoid):
     try:
         query = f"https://www.youtube.com/watch?v={videoid}"
         results = VideosSearch(query, limit=1)
         for result in (await results.next())["result"]:
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
         return thumbnail
-    except Exception as e:Bct music 
+    except Exception as e:Bctmusic 
         return f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
 
 
@@ -20,5 +20,5 @@ async def gen_qthumb(vidid):
         for result in (await results.next())["result"]:
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
         return thumbnail
-    except Exception as e:Bct music 
+    except Exception as e:Bctmusic 
         return f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
